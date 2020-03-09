09a605e1e412016be6807fb2a7c9ac9631c78220
Scott Harlan <seharlan@gmail.com>
2020-03-09 15:48:05 -0700

    index.js added

---
+++ index.js
@@ -0,0 +1,19 @@
+const { isNumber,
+    isString,
+    isBoolean,
+    isArray,
+    isObject,
+    isFunction,
+    castToNumber,
+    castToString,
+    castToBoolean } = require('./lib/types.js');
+
+console.log(isNumber('3'));
+console.log(isString('string'));
+console.log(isBoolean(true));
+console.log(isArray([]));
+console.log(isObject({}));
+console.log(isFunction(() => {}));
+console.log(castToNumber('2'));
+console.log(castToString(55));
+console.log(castToBoolean('true'));
